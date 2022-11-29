c21db013ca85a588f6585f32c37f2702b9b63d46
Abiyy <siblal001@gmail.com>
2022-11-29 23:28:44 +0700

    Axios null

--- src/components/StripeButton.tsx
+++ src/components/StripeButton.tsx
@@ -10,23 +10,7 @@ function StripeButton(props: Props) {
   const priceForStripe = props.price * 100
   const publishableKey = "pk_test_51M9W07AjbMbDfpGhvuCXLtbO0C12gbityqRMjUiIMoSboaMt4Zb7xjVE7ra5nReG3G6D28YpXmom6TkDSH054MHJ00V8QxKVQ0"
 
-  const onToken = (token: any) => {
-    axios({
-      url: "payment",
-      method: "post",
-      data: {
-        amount: priceForStripe,
-        token: token,
-      },
-    })
-      .then((response) => {
-        alert("succesful payment")
-      })
-      .catch((error) => {
-        console.log("Payment Error: ", error)
-        alert("There was an issue with your payment! Please make sure you use the provided credit card.")
-      })
-  }
+  const onToken = (token: any) => {}
   return <StripeCheckout label="Pay Now" name="E-Store Proj" billingAddress shippingAddress image="https://svgur.com/i/oSN.svg" description={`Your total is $${props.price}`} amount={priceForStripe} panelLabel="Pay Now" token={onToken} stripeKey={publishableKey} />
 }
 
